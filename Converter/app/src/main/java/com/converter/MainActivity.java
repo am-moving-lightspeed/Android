1311a2affff7458f2ac3ec67c99462915502fa89
am.moving.lightspeed <am.moving.lightspeed@gmail.com>
2020-12-28 22:10:17 +0300

    Minor fixes.

--- Converter/app/src/main/java/com/converter/MainActivity.java
+++ Converter/app/src/main/java/com/converter/MainActivity.java
@@ -1,21 +1,57 @@
 package com.converter;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    //region Classes
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new CategoryFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return CATEGORIES_AMOUNT;
+        }
+    }
+    //endregion
+
+    //region Constants
+    private static final int CATEGORIES_AMOUNT = 3;
+    //endregion
+
     //region Variables
+    private ViewPager       _viewPager;
+    private PagerAdapter    _pagerAdapter;
+
     private EditText    _activeInput;
-    private EditText    _etValue1;
-    private EditText    _etValue2;
-    private EditText    _etValue3;
+    private EditText    _etGram;
+    private EditText    _etKilo;
+    private EditText    _etTon;
 
     private Button      _bNum0;
     private Button      _bNum1;
@@ -40,9 +76,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //region Find by id
-        _etValue1 = findViewById(R.id.inputField1);
-        _etValue2 = findViewById(R.id.inputField2);
-        _etValue3 = findViewById(R.id.inputField3);
+        _etGram = findViewById(R.id.inputField1);
+        _etKilo = findViewById(R.id.inputField2);
+        _etTon = findViewById(R.id.inputField3);
 
         _bNum0      = findViewById(R.id.kbNum0);
         _bNum1      = findViewById(R.id.kbNum1);
@@ -57,9 +93,13 @@ public class MainActivity extends AppCompatActivity {
         _bDot       = findViewById(R.id.kbDot);
         _bEraseLast = findViewById(R.id.kbEraseLast);
         _bEraseAll  = findViewById(R.id.kbEraseAll);
+
+        _viewPager      = findViewById(R.id.categoryPager);
+        _pagerAdapter   = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        _viewPager.setAdapter(_pagerAdapter);
         //endregion
 
-        _activeInput = _etValue1;
+        _activeInput = _etGram;
 
         //region Events
         _bEraseLast.setOnClickListener(new View.OnClickListener() {
@@ -100,6 +140,56 @@ public class MainActivity extends AppCompatActivity {
                     _activeInput.append(".");
                 }
             }
+        });
+
+        _activeInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String buffer;
+                double value;
+
+                if (s.charAt(s.length() - 1) == '.') {
+                    buffer = new String(s.toString() + "0");
+                }
+                else {
+                    buffer = new String(s.toString());
+                }
+
+                value = Double.parseDouble(buffer);
+
+                if (_activeInput.equals(_etGram)) {
+                    _etKilo.setText(
+                        String.format(Locale.US, "%f", value / 1000.0)
+                    );
+                    _etTon.setText(
+                        String.format(Locale.US, "%f", value / 1000.0 / 1000.0)
+                    );
+                }
+                else if (_activeInput.equals(_etKilo)) {
+                    _etGram.setText(
+                        String.format(Locale.US, "%f", value * 1000.0)
+                    );
+                    _etTon.setText(
+                        String.format(Locale.US, "%f", value / 1000.0)
+                    );
+                }
+                else {
+                    _etGram.setText(
+                        String.format(Locale.US, "%f", value * 1000.0 * 1000.0)
+                    );
+                    _etKilo.setText(
+                        String.format(Locale.US, "%f", value * 1000.0)
+                    );
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
         });
         //endregion
     }
